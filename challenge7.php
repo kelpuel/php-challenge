5b940cd1df095b6e6091a46295a2171184fa9374
alexmdn <mradn.paray@gmail.com>
2023-07-06 14:50:15 +0200

    Challenge 7 PHP

---
+++ challenge7.php
@@ -0,0 +1,23 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Alex" => [
+        "city" => "Paray",
+        "weapon" => "Boomerang"
+    ]
+];
+var_dump($characters)
+
+// We write code here
+?>
